f0e736c5c7c62c71c99879c03d7a16366050356a
Mansoor Sayed <mansoor@mansoorsayed.com>
2013-09-14 13:05:26 -0600

    Adding documentation

--- sleep.go
+++ sleep.go
@@ -9,8 +9,10 @@ import (
 	"reflect"
 )
 
-//convenience
+//Convenient access to bson.M
 type M bson.M
+
+//Convenient access to bson.D
 type D bson.D
 
 type Sleep struct {
@@ -44,7 +46,10 @@ func (z *Sleep) Register(schema interface{}, collectionName string) {
 
 // Find starts a chainable *Query value
 // This function passes the supplied value to mgo.Collection.Find
-// To borrow from the mgo docs: "The document(argument) may be a map or a struct value capable of being marshalled with bson. The map may be a generic one using interface{} for its key and/or values, such as bson.M, or it may be a properly typed map. Providing nil as the document is equivalent to providing an empty document such as bson.M{}".
+// To borrow from the mgo docs: "The document(argument) may be a map or a struct value capable of being marshalled with bson.
+// The map may be a generic one using interface{} for its key and/or values, such as bson.M, or it may be a properly typed map.
+// Providing nil as the document is equivalent to providing an empty document such as bson.M{}".
+//
 // Further reading: http://godoc.org/labix.org/v2/mgo#Collection.Find
 func (z *Sleep) Find(query interface{}) *Query {
 	return &Query{query: query, z: z,
@@ -75,9 +80,9 @@ func (z *Sleep) FindId(id interface{}) *Query {
 }
 
 // Create conditions an instance of the model to become a document.
-// What it means in pratical terms is that Create sets a value for the schema's Model(Sleep.Model) anonymous field. This will allow Sleep to work with the value
-// calling this function is only necessary when wishing to create documents "manually".
-// It is not necessary to call this function on a value that will be holding the result of a query
+// What it means in pratical terms is that Create sets a value for the schema's Model(Sleep.Model) anonymous field. This will allow Sleep to work with the value as a document.
+// Calling this function is only necessary when wishing to create documents "manually".
+// It is not necessary to call this function on a value that will be holding the result of a query; Sleep will do that.
 // After a document is created with this function, the document will expose all of the public methods and fields of the Sleep.Model struct as its own.
 func (z *Sleep) Create(doc interface{}) {
 	typ := reflect.TypeOf(doc).Elem()
@@ -94,6 +99,8 @@ func (z *Sleep) Create(doc interface{}) {
 	idField.Set(reflect.ValueOf(id))
 }
 
+// C gives access to the underlying *mgo.Collection value for a model.
+// The model name is case sensitive.
 func (z *Sleep) C(model string) (*mgo.Collection, bool) {
 	m, ok := z.models[model]
 	c := m.C
